0cf5dd02db739a5c22074181fb7e6a6abd9577f7
Kyle Chadwick <kylepchad@gmail.com>
2017-10-25 22:39:50 -0700

    initial commit

---
+++ FloorIsLava/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  FloorIsLava
+//
+//  Created by Kyle Chadwick on 10/25/17.
+//  Copyright © 2017 Kyle Chadwick. All rights reserved.
+//
+
+import UIKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet weak var sceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        self.configuration.planeDetection = .horizontal
+        self.sceneView.session.run(configuration)
+        self.sceneView.delegate = self
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else {return}
+        print("new flat surface detected, new ARPlaneAnchor added")
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else {return}
+        print("updating floor's anchor")
+    }
+    
+}
